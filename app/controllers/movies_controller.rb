6ad4deefa1730ad9a7d500897bf5d2637aadcd54
Stuart Frew stuart.s.frew@gmail.com <stuart.s.frew@gmail.com>
2023-05-09 20:26:37 +0100

    added TMDB api key to .env file

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,8 @@ require 'open-uri'
 require 'json'
 
 class MoviesController < ApplicationController
-
   def index
-    url = 'https://tmdb.lewagon.com/movie/top_rated'
+    url = "https://api.themoviedb.org/3/search/movie?api_key=#{ENV['TMDB_API_KEY']}&query=Jack+Reacher"
     data = JSON.parse(URI.open(url).read)
     @results = data['results']
   end
